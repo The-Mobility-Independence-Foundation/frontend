f8bb1c2c4a9f54ea5fc14b5b62fb2acbddf69f73
scf2054 <frosts1125@gmail.com>
2025-01-24 14:52:04 -0500

    Added functionality to pressing on the buttons

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import "../styles/pagination-component.css";
 
 interface Props {
@@ -9,32 +10,59 @@ interface Props {
 
 export default function Pagination({numberOfItems, itemsPerPage}: Props) {
   // default page data
-  let startIndex = 0;
+  const [startIndex, setStartIndex] = useState(0);
+  const [page, setPage] = useState(1);
   let endIndex = numberOfItems < itemsPerPage ? numberOfItems-1 : itemsPerPage-1;
-  let page = 1;
-
   const numberOfPages = Math.ceil(numberOfItems / itemsPerPage);
-  let boxes: (number | string)[] = numberOfPages > 5 
+  const [boxes, setBoxes] = useState(numberOfPages > 5 
     ? [1, 2, "...", numberOfPages-1, numberOfPages]
-    : Array.from({length: numberOfPages}, (_, index) => index + 1);
+    : Array.from({length: numberOfPages}, (_, index) => index + 1));
+
+  const changePage = (newPage: number) => {
+    setPage(newPage);
+    setStartIndex((newPage - 1) * itemsPerPage);
+    let newBoxes = [];
+    let ellipsisAdded = false;
+    for(let i = 1; i <= numberOfPages; i++) {
+      if(i == 1 || i == 2) {
+        newBoxes.push(i);
+        ellipsisAdded = false;
+      } else if(i == newPage - 1 || i == newPage || i == newPage + 1) {
+        newBoxes.push(i);
+        ellipsisAdded = false;
+      } else if(i == numberOfPages - 1 || i == numberOfPages) {
+        newBoxes.push(i);
+        ellipsisAdded = false;
+      } else if(!ellipsisAdded) {
+        newBoxes.push("...");
+        ellipsisAdded = true;
+      }
+    }
+    setBoxes(newBoxes);
+  };
+
+  // resetBoxes();
 
   return <div className="flex">
-    <div className="box arrow">
+    <div className="box" style={{pointerEvents: page == 1 ? "none" : "initial"}}
+         onClick={() => changePage(page - 1)}>
       <svg width="25" height="25" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 4L9 11L4.5 7.5L9 4Z" fill="currentColor"></path></svg>
     </div>
-    {boxes.map(num => {
+    {boxes.map((num, index) => {
       if(num == "...") {
-        return <div className="relative mr-[5px] ml-[5px] mt-[3px] text-[20px] text-center font-semibold h-[40px] w-[18px]">
+        return <div key={"ellipsis-" + index} className="relative mr-[5px] ml-[5px] mt-[3px] text-[20px] text-center font-semibold h-[40px] w-[18px]">
           <span className="absolute bottom-0 left-0">{num}</span>
         </div>;
       }
-      return <div className="box"
-                  style={{backgroundColor: num == page ? "#EBEBEB" : "white"}}>
+      return <div className="box" key={num}
+                  style={{backgroundColor: num == page ? "#EBEBEB" : "white", pointerEvents: page == num ? "none" : "initial"}}
+                  onClick={() => changePage(num as number)}>
                     {num}
               </div>;
       })
     }
-    <div className="box arrow">
+    <div className="box" style={{pointerEvents: page == numberOfPages ? "none" : "initial"}}
+         onClick={() => changePage(page + 1)}>
       <svg width="25" height="25" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 11L6 4L10.5 7.5L6 11Z" fill="currentColor"></path></svg>
     </div>
   </div>
