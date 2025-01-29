d22fb962428ab63377e8d1f99c04143d24a806b0
scf2054 <frosts1125@gmail.com>
2025-01-29 14:18:51 -0500

    Removed Dropdown and replaced magnifying image with svg

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Image from 'next/image';
 import "../styles/search-component.css";
-import { ChangeEvent, SetStateAction, SyntheticEvent, useState } from "react";
+import { useState } from "react";
 
 interface Props {
   apiRoute: string;
@@ -45,13 +44,9 @@ export default function Search({apiRoute, placeholderText, newButtonText, newBut
         value={searchQuery}
         onChange={handleSearchQueryChange}
       />
-      <Image 
-        src="/images/search.svg"
-        height={35}
-        width={35}
-        alt="Search"
-        className="absolute right-[5px]"
-      />
+      <svg className="absolute right-[5px]" width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+      </svg>
     </div>
     {filter ?
       <button>
