88edf54f384a9f42487a7f1d43891ccf0ea41537
scf2054 <frosts1125@gmail.com>
2025-01-29 14:24:07 -0500

    Replaced input with Input from SHADCN

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -2,6 +2,7 @@
 
 import "../styles/search-component.css";
 import { useState } from "react";
+import { Input } from "@/components/ui/input"
 
 interface Props {
   apiRoute: string;
@@ -37,12 +38,11 @@ export default function Search({apiRoute, placeholderText, newButtonText, newBut
         </button>
     : ""}
     <div className="flex max-w-[700px] w-[50%] relative items-center">
-      <input 
-        type="text" 
-        className="border-black w-full"
-        placeholder={placeholderText || "Search"}
-        value={searchQuery}
-        onChange={handleSearchQueryChange}
+      <Input type="text"
+             className="bg-white"
+             placeholder={placeholderText || "Search"}
+             value={searchQuery}
+             onChange={handleSearchQueryChange}
       />
       <svg className="absolute right-[5px]" width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
