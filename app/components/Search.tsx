1919b2aa3473764e468713bb87edee0ef0fb8ad0
scf2054 <frosts1125@gmail.com>
2025-01-29 15:41:10 -0500

    Added in form control for submitting the search query

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -3,6 +3,11 @@
 import "../styles/search-component.css";
 import { useState } from "react";
 import { Input } from "@/components/ui/input"
+import { Form, FormProvider, useForm } from "react-hook-form";
+import { FormControl, FormField, FormItem } from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   apiRoute: string;
@@ -10,14 +15,20 @@ interface Props {
   newButtonText?: string;
   newButtonEvent?: (clicked: boolean) => void;
   filter?: boolean;
+  defaultQuery?: string;
 }
 
-export default function Search({apiRoute, placeholderText, newButtonText, newButtonEvent, filter}: Props) {
-  const [searchQuery, setSearchQuery] = useState("");
+const formSchema = z.object({
+  query: z.string()
+})
 
-  const handleSearchQueryChange = (event: any) => {
-    setSearchQuery(event.target.value);
-  }
+export default function Search({apiRoute, placeholderText, newButtonText, newButtonEvent, filter, defaultQuery}: Props) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      query: defaultQuery || "",
+    }
+  });
 
   const handleNewButtonClick = (event: any) => {
     if(newButtonEvent) {
@@ -25,11 +36,15 @@ export default function Search({apiRoute, placeholderText, newButtonText, newBut
     }
   }
 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   // TODO: API route hook up -> sends information back to parent
   // TODO: Add filter component once finished
 
   return <div 
-    className="w-full py-[15px] px-[10%] flex place-content-around"
+    className="w-full py-[15px] px-[2%] flex place-content-around"
     style={{backgroundColor: "#D1D5DB"}}
   >
     {newButtonEvent ? 
@@ -37,17 +52,32 @@ export default function Search({apiRoute, placeholderText, newButtonText, newBut
         + {newButtonText || "New"}
         </button>
     : ""}
-    <div className="flex max-w-[700px] w-[50%] relative items-center">
-      <Input type="text"
-             className="bg-white"
-             placeholder={placeholderText || "Search"}
-             value={searchQuery}
-             onChange={handleSearchQueryChange}
-      />
-      <svg className="absolute right-[5px]" width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
-      </svg>
-    </div>
+    <FormProvider {...form}>
+      <form 
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="max-w-[700px] w-[50%] h-full"
+      >
+        <FormField
+          control={form.control}
+          name="query"
+          render={({ field }) => (
+            <FormItem className="flex relative items-center">
+              <FormControl>
+                  <Input 
+                    {...field}
+                    className="bg-white"
+                    placeholder={placeholderText || "Search"}
+                  />
+              </FormControl>
+              <svg className="absolute right-[5px] !mt-[0px]" width="27" height="27" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+              </svg>
+            </FormItem>
+          )}
+        >
+        </FormField>
+      </form>
+    </FormProvider>
     {filter ?
       <button>
         Filter
