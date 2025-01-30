1213a7ea6003cf15cb980b80ba42687cd7d5e4d8
scf2054 <frosts1125@gmail.com>
2025-01-30 12:11:02 -0500

    Used .env file instead

--- app/services/backend.service.ts
+++ app/services/backend.service.ts
@@ -1,30 +1,28 @@
 import axios from "axios";
 
-const BASE_URL = "https://jsonplaceholder.typicode.com"
-
 const fetch = async (route: string) => {
-  const request = axios.get(`${BASE_URL}${route}`);
+  const request = axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}${route}`);
   return request
     .then(response => response.data)
     .catch(error => console.log(error));
 }
 
 const create = async (route: string, data: any) => {
-  const request = axios.post(`${BASE_URL}${route}`, data)
+  const request = axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}${route}`, data)
   return request
     .then(response => response.data)
     .catch(error => console.log(error));
 }
 
 const update = async (route: string, data: any) => {
-  const request = axios.put(`${BASE_URL}${route}`, data);
+  const request = axios.put(`${process.env.NEXT_PUBLIC_BASE_URL}${route}`, data);
   return request
     .then(response => response.data)
     .catch(error => console.log(error));
 }
 
 const remove = async (route: string) => {
-  const request = axios.delete(`${BASE_URL}${route}`);
+  const request = axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}${route}`);
   return request
     .then(response => response.data)
     .catch(error => console.log(error));
