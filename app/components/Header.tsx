fba8669f5226bcc1a388dea7e88919069aa0c2d1
scf2054 <frosts1125@gmail.com>
2025-02-06 12:13:57 -0500

    Added format or routes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,13 @@
 export default function Header() {
-  return <>
-    <img src="/assets/Header Logo.png" alt="logo"></img>
-  </>
+  return <div className="bg-[#002856] py-[0.5rem] w-full flex justify-around items-center font-bold text-white text-xs">
+    <img src="/assets/Header Logo.png" alt="logo" className="w-[20%]"></img>
+    <div className="space-x-[1rem]">
+      <a href="/listings">Public Listings</a>
+      <a href="/forum">Forum</a>
+      <a href="/messages">Private Messages</a>
+      <a href="/inventories">My Inventories</a>
+      <a href="/listings">My Listings</a>
+    </div>
+    <a href="/account">Account</a>
+  </div>
 }
