cbe12656f4c39ac5c163d4a6db0492db25eed258
scf2054 <frosts1125@gmail.com>
2025-02-06 12:49:05 -0500

    Route buttons now actually route to new components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+interface Link {
+  route: string;
+  title: string;
+}
+
 export default function Header() {
+  const userID = 1;
+
+  const links: Link[] = [
+    {route: "/listings", title: "Public Listings"},
+    {route: "/forum", title: "Forum"},
+    {route: `/messages/${userID}`, title: "Private Messages"},
+    {route: `/inventories/${userID}`, title: "My Inventories"},
+    {route: `/listings/${userID}`, title: "My Listings"},
+  ]
+
   return <div className="bg-[#002856] py-[0.5rem] w-full flex justify-around items-center font-bold text-white text-xs">
     <img src="/assets/Header Logo.png" alt="logo" className="w-[20%]"></img>
-    <div className="space-x-[1rem]">
-      <a href="/listings">Public Listings</a>
-      <a href="/forum">Forum</a>
-      <a href="/messages">Private Messages</a>
-      <a href="/inventories">My Inventories</a>
-      <a href="/listings">My Listings</a>
-    </div>
+    <nav className="space-x-[1rem]">
+      {links.map(link => 
+        <NavLink 
+          to={link.route}
+          className={({ isActive }) => 
+            isActive ? "text-[#009D4F]" : "text-white"
+          }
+        >{link.title}</NavLink>
+      )}
+    </nav>
     <a href="/account">Account</a>
   </div>
 }
---
+++ app/pages/PrivateMessages.tsx
@@ -0,0 +1,3 @@
+export default function PrivateMessages() {
+  return <>Private Messages!</>
+}
