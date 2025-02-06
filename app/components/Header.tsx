d678d56f2ddc8312463a29676d575841de091ee7
scf2054 <frosts1125@gmail.com>
2025-02-06 11:25:40 -0500

    Stubbed out header component with image logo

---
+++ app/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return <>
+    <img src="/assets/Header Logo.png" alt="logo"></img>
+  </>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import PaginationComponent, { PageChangeEvent } from "./components/Pagination";
+import Header from "./components/Header";
 
 export default function Home() {
-  const onPageChange = (event: PageChangeEvent) => {
-    console.log(event);
-  }
-
+  
   return <div className="w-screen h-screen">
-    <PaginationComponent count={50} total={1000} hasNext={true} nextToken={251} onPageChange={async (event) => onPageChange(event)} />
+    <Header></Header>
   </div>
 }
