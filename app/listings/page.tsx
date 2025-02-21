745253740c5bbb2c605b6761795b43fb74cd0271
scf2054 <frosts1125@gmail.com>
2025-02-21 11:33:15 -0500

    Hooked up search to change the current data being displayed on search

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -4,15 +4,16 @@ import { useSearchParams } from "next/navigation"
 import Search from "../components/Search";
 import { useState } from "react";
 import { FilterComponentType } from "../types/FilterTypes";
+import { Listings } from "../models/Listings";
 
 export default function PublicListings() {
-  const [listings, setListings] = useState([]);
+  const [listings, setListings] = useState<Listings[]>([]);
 
   const params = useSearchParams();
   const userID = params.get("u_id");
 
   const receiveListings = (data: any[]) => {
-    console.log(data);
+    setListings(data as Listings[]);
   }
 
   return <>
