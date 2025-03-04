28c4c8470a5c3692ecc92b235401af5de492ede5
scf2054 <frosts1125@gmail.com>
2025-03-04 11:47:41 -0500

    Layout of recommended listings

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -39,65 +39,81 @@ export default function ListingPage() {
     <>
       {listing && (
         <>
-          {/** TOP BAR */}
-          <div className="w-full flex justify-between p-[0.65rem] bg-[#F4F4F5] drop-shadow-md overflow-hidden">
-            <div>
-              <h2>{listing.title}</h2>
-              <p className="max-w-[20rem] text-sm">{listing.inventoryItem.part.description}</p>
-              <div className="flex mt-[1rem]">
-                <div>
-                  <h5>{listing.inventoryItem.part.partNumber}</h5>
-                  <p className="mt-[revert]">
-                    {listing.inventoryItem.part.model}
-                  </p>
-                  <p>{listing.inventoryItem.part.partType}</p>
+          <div className="flex flex-col h-screen">
+            {/** TOP BAR */}
+            <div className="w-full min-h-min flex justify-between p-[0.65rem] bg-[#F4F4F5] drop-shadow-md overflow-hidden">
+              <div className="mr-[0.25rem]">
+                <h2>{listing.title}</h2>
+                <p className="max-w-[20rem] text-sm">
+                  {listing.inventoryItem.part.description}
+                </p>
+                <div className="flex mt-[1rem]">
+                  <div>
+                    <h5>{listing.inventoryItem.part.partNumber}</h5>
+                    <p className="mt-[revert]">
+                      {listing.inventoryItem.part.model}
+                    </p>
+                    <p>{listing.inventoryItem.part.partType}</p>
+                  </div>
+                  <ul className="ml-[3rem] max-h-[6rem] overflow-y-auto">
+                    {Object.keys(listing.attributes).map((key) => (
+                      <li
+                        className="mb-[0.25rem]"
+                        key={`${key}: ${listing.attributes[key]}`}
+                      >
+                        - {key}: {listing.attributes[key]}
+                      </li>
+                    ))}
+                  </ul>
                 </div>
-                <ul className="ml-[3rem] max-h-[6rem] overflow-y-auto">
-                  {Object.keys(listing.attributes).map((key) => (
-                    <li
-                      className="mb-[0.25rem]"
-                      key={`${key}: ${listing.attributes[key]}`}
-                    >
-                      - {key}: {listing.attributes[key]}
-                    </li>
-                  ))}
-                </ul>
               </div>
+              <div className="flex flex-col my-[1rem] mx-[0.25rem] justify-between">
+                <div>
+                  <h5>{listing.inventoryItem.inventory.organization.name}</h5>
+                  <p>{listing.inventoryItem.inventory.organization.email}</p>
+                  <p>
+                    {listing.inventoryItem.inventory.organization.phoneNumber}
+                  </p>
+                </div>
+                <Link
+                  href={`/messages?u_id=${listing.inventoryItem.inventory.organization.id}`}
+                  className="w-full"
+                >
+                  <button className="w-full button">Message</button>{" "}
+                  {/**TODO: routes to specified user pv */}
+                </Link>
+              </div>
+              <div className="flex flex-col my-[1rem] mx-[0.25rem] justify-between">
+                <div>
+                  <h5>{listing.inventoryItem.inventory.name}</h5>
+                  <p>{listing.inventoryItem.inventory.location}</p>
+                </div>
+                <div>
+                  <p className="w-max mx-auto">Quantity Available:</p>
+                  <h5 className="w-min mx-auto">{listing.quantity}</h5>
+                </div>
+                <button
+                  className="button"
+                  onClick={() => setCreateOrderModalIsOpen(true)}
+                >
+                  Create Order
+                </button>
+              </div>
+              {images && <ImageCarousel className="my-auto mx-[0.25rem]" images={images} />}
             </div>
-            <div className="flex flex-col my-[1rem] justify-between">
-              <div>
-              <h5>{listing.inventoryItem.inventory.organization.name}</h5>
-                <p>{listing.inventoryItem.inventory.organization.email}</p>
-                <p>{listing.inventoryItem.inventory.organization.phoneNumber}</p>
-              </div>
-              <Link href={`/messages?u_id=${listing.inventoryItem.inventory.organization.id}`} className="w-full">
-                <button className="w-full button">Message</button>{" "}
-                {/**TODO: routes to specified user pv */}
-              </Link>
+            {/** RECOMMENDED LISTINGS */}
+            <div className="bg-[#D8EAF6] w-full flex-1 p-[0.65rem]">
+              <h4 className="mb-[1rem]">Recommended Listings</h4>
+              
             </div>
-            <div className="flex flex-col my-[1rem] justify-between">
-              <div>
-                <h5 >{listing.inventoryItem.inventory.name}</h5>
-                <p>{listing.inventoryItem.inventory.location}</p>
-              </div>
-              <div>
-                <p className="w-max mx-auto">Quantity Available:</p>
-                <h5 className="w-min mx-auto">{listing.quantity}</h5>
-              </div>
-              <button className="button" onClick={() => setCreateOrderModalIsOpen(true)}>
-                Create Order
-              </button>
-            </div>
-            {images && <ImageCarousel className="my-auto" images={images} />}
           </div>
-          {/** RECOMMENDED LISTINGS */}
-          <div></div>
+          {/** ETC */}
           <Modal
             isOpen={createOrderModalIsOpen}
             onClose={() => setCreateOrderModalIsOpen(false)}
           >
-            <CreateOrder 
-              listing={listing} 
+            <CreateOrder
+              listing={listing}
               listingImages={images}
               onClose={() => setCreateOrderModalIsOpen(false)}
             />
