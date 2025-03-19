871cb0ec7c6f3130e10be917713f53b7c5235934
scf2054 <frosts1125@gmail.com>
2025-03-19 13:27:54 -0400

    Added name input and select from SHADCN

--- app/components/modals/CreateInventoryItem.tsx
+++ app/components/modals/CreateInventoryItem.tsx
@@ -7,6 +7,9 @@ import backendService from "@/app/services/backend.service";
 import { useEffect, useState } from "react";
 import { PartData } from "@/app/models/Part";
 import { ModelData } from "@/app/models/Model";
+import { FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 interface CreateInventoryItemModalProps {
   onClose: () => void,
@@ -50,7 +53,7 @@ export default function CreateInventoryItemModal({onClose, organizationID, inven
     attributes: z
       .string()
   });
-  const createInventoryForm = useForm<z.infer<typeof createInventoryItemFormSchema>>({
+  const createInventoryItemForm = useForm<z.infer<typeof createInventoryItemFormSchema>>({
     resolver: zodResolver(createInventoryItemFormSchema)
   });
 
@@ -74,12 +77,90 @@ export default function CreateInventoryItemModal({onClose, organizationID, inven
     onClose();
   }
   
-  return <div className="min-w-[25rem]">
+  return <div className="max-w-[35rem] w-[35rem]">
     <ModalHeader title="Create a New Inventory Item" onClose={onClose} />
     <ModalBody>
-      <FormProvider {...createInventoryForm}>
-        <form onSubmit={createInventoryForm.handleSubmit(onFormSubmit)}>
-
+      <FormProvider {...createInventoryItemForm}>
+        <form onSubmit={createInventoryItemForm.handleSubmit(onFormSubmit)}>
+          <FormField
+            control={createInventoryItemForm.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input 
+                    {...field}
+                    type="string"
+                    required={true}
+                    placeholder="Name"
+                    className="mb-[0.75rem]"
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={createInventoryItemForm.control}
+            name="partID"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                <Select
+                  onValueChange={value => field.onChange(value)}
+                  required={true}
+                >
+                  <SelectTrigger className="mb-[0.75rem]">
+                    <SelectValue placeholder="Select a Part" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Parts</SelectLabel>
+                      {parts.map(part => 
+                        <SelectItem 
+                          key={part.id}
+                          value={part.id.toString()}
+                        >
+                          {part.name}
+                        </SelectItem>
+                      )}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={createInventoryItemForm.control}
+            name="modelID"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                <Select
+                  onValueChange={value => field.onChange(value)}
+                  required={true}
+                >
+                  <SelectTrigger className="mb-[0.75rem]">
+                    <SelectValue placeholder="Select a Model" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Models</SelectLabel>
+                      {models.map(model => 
+                        <SelectItem 
+                          key={model.id}
+                          value={model.id.toString()}
+                        >
+                          {model.name}
+                        </SelectItem>
+                      )}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
         </form>
         
       </FormProvider>
