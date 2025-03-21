11ba8cc23861302b2c6c3814e9026decda5459f3
scf2054 <frosts1125@gmail.com>
2025-03-20 21:15:16 -0400

    Fixed styling

--- app/components/CreateListing.tsx
+++ app/components/CreateListing.tsx
@@ -4,9 +4,18 @@ import { InventoryItemData } from "../models/InventoryItem";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import RadioButton from "./RadioButton";
+import { Label } from "@/components/ui/label";
 
 // TODO: second half
 export default function CreateListing() {
@@ -17,56 +26,56 @@ export default function CreateListing() {
   // TODO: API call for grabbing all items from all inventories
 
   const createListingSchema = z.object({
-    inventoryItemID: z
-      .number({
-        required_error: "Select an item from your inventories"
-      }),
+    inventoryItemID: z.number({
+      required_error: "Select an item from your inventories",
+    }),
     quantity: z
       .number({
-        required_error: "Include how much you would like to list"
+        required_error: "Include how much you would like to list",
       })
       .min(1),
-    active: z
-      .boolean({
-        required_error: "Indicate whether or not this listing will be active upon creation"
-      }),
-    attachment: z
-      .string()
+    active: z.boolean({
+      required_error:
+        "Indicate whether or not this listing will be active upon creation",
+    }),
+    attachment: z.string(),
   });
 
   const createListingForm = useForm<z.infer<typeof createListingSchema>>({
     resolver: zodResolver(createListingSchema),
     defaultValues: {
-      active: true
-    }
+      active: true,
+    },
   });
 
   const onFormSubmit = (values: z.infer<typeof createListingSchema>) => {
     console.log(values);
     // TODO: POST call for creating a listing
-  }
+  };
 
   const onInventoryItemChange = (value: string, field: any) => {
     field.onChange(value);
-    inventoryItems.forEach(item => {
-      if(item.id.toString() == value) {
+    inventoryItems.forEach((item) => {
+      if (item.id.toString() == value) {
         setQuantityAvailable(item.quantity - item.publicCount);
       }
     });
-  }
+  };
 
   const onActiveChange = (value: number, field: any) => {
     field.onChange(value == 1);
     setActiveButton(value);
-  }
+  };
 
-  return <div className="w-full bg-white p-5" id="filters">
+  return (
+    <div className="w-full bg-white p-5" id="filters">
       <FormProvider {...createListingForm}>
-        <form 
+        <form
           onSubmit={createListingForm.handleSubmit(onFormSubmit)}
-          className="flex justify-around w-min 
-                    max-md:flex-col max-md:items-center max-md:w-full"
+          className="flex flex-grow"
         >
+          <div className="flex justify-around items-center w-full
+                    max-md:flex-col">
           <div className="w-[25rem]">
             <FormField
               control={createListingForm.control}
@@ -74,45 +83,55 @@ export default function CreateListing() {
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                  <Select
-                    onValueChange={value => onInventoryItemChange(value, field)}
-                    required={true}
-                  >
-                    <SelectTrigger className="mb-[0.75rem]">
-                      <SelectValue placeholder="Select an item from your inventories" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Inveotry Item</SelectLabel>
-                        {inventoryItems.map(item => 
-                          <SelectItem 
-                            key={item.id}
-                            value={item.id.toString()}
-                          >
-                              {item.name} [{item.inventory.name}]
-                          </SelectItem>
-                        )}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
+                    <>
+                      <Select
+                        onValueChange={(value) =>
+                          onInventoryItemChange(value, field)
+                        }
+                        required={true}
+                      >
+                        <SelectTrigger className="mb-[0.75rem]">
+                          <SelectValue placeholder="Select an item from your inventories" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectLabel>Inveotry Item</SelectLabel>
+                            {inventoryItems.map((item) => (
+                              <SelectItem
+                                key={item.id}
+                                value={item.id.toString()}
+                              >
+                                {item.name} [{item.inventory.name}]
+                              </SelectItem>
+                            ))}
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </>
                   </FormControl>
                 </FormItem>
               )}
             />
-            <p className="flex items-center">Quantity Available: <h5 className="ml-[1rem]">{quantityAvailable > -1 ? quantityAvailable : "N/A"}</h5></p>
+            <div className="flex items-center">
+              <p>Quantity Available: </p>
+              <h5 className="ml-[1rem]">
+                {quantityAvailable > -1 ? quantityAvailable : "N/A"}
+              </h5>
+            </div>
             <FormField
               control={createListingForm.control}
               name="quantity"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input 
+                    <Input
                       {...field}
                       type="number"
                       min="1"
                       disabled={quantityAvailable == -1}
                       placeholder="Quantity Listed"
                       className="max-w-[10rem] mb-5"
+                      id="quantity"
                     />
                   </FormControl>
                 </FormItem>
@@ -124,18 +143,40 @@ export default function CreateListing() {
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <RadioButton 
+                    <RadioButton
                       label1="Active"
                       label2="Inactive"
                       selected={activeButton}
-                      onChange={newSelected => onActiveChange(newSelected, field)}
+                      onChange={(newSelected) =>
+                        onActiveChange(newSelected, field)
+                      }
                     />
                   </FormControl>
                 </FormItem>
               )}
             />
           </div>
+          <div
+            className="w-[2px] h-[80%] bg-[#d9d9d9] rounded
+                      max-md:w-[80%] max-md:h-[2px] max-md:my-[1rem]"
+          />
+          <div className="flex flex-col h-full
+                          ">
+            <FormField
+              control={createListingForm.control}
+              name="attachment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} type="file" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          </div>
         </form>
       </FormProvider>
     </div>
+  );
 }
