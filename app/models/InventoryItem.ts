631e71d02a9dbd38f6c2105ecb7f6e8369f58658
scf2054 <frosts1125@gmail.com>
2025-03-24 10:17:55 -0400

    Simplified attribute algorithms

--- app/models/InventoryItem.ts
+++ app/models/InventoryItem.ts
@@ -29,27 +29,16 @@ export interface InventoryItemData {
 }
 
 export function attributesToString(attributes: { [key: string]: any }) {
-  let attributesAsString = "";
-  const attributeKeys = Object.keys(attributes);
-  for(let i = 0; i < attributeKeys.length; i++) {
-    let attributeKey = attributeKeys[i];
-    attributesAsString += `${attributeKey}:${attributes[attributeKey]}`;
-    if(i < attributeKeys.length-1) {
-      attributesAsString += "\n";
-    }
-  }
-  return attributesAsString;
+  return Object.keys(attributes)
+    .map((attributeKey) => `${attributeKey}: ${attributes[attributeKey]}`)
+    .join("\n");
 }
 
 export function stringToAttributes(string: string) {
-  if(!ATTRIBUTES_STRING_REGEX.test(string)) {
+  if (!ATTRIBUTES_STRING_REGEX.test(string)) {
     throw new Error("String isn't in proper format.");
   }
-  const attributes: { [key: string]: any } = {};
-  const attributesList = string.split("\n");
-  for(let pair of attributesList) {
-    const keyValue = pair.split(":");
-    attributes[keyValue[0]] = keyValue[1];
-  }
-  return attributes;
+  return new Map(
+    string.split("\n").map((pair) => [pair.split(":")[0], pair.split(":")[1]])
+  );
 }
