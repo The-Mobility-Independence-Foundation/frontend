59b6f3f0fb8cc9f423eb4b03d700e20c9b59014f
scf2054 <frosts1125@gmail.com>
2025-03-27 12:03:06 -0400

    Fixed InventoryItem Model

--- app/models/InventoryItem.ts
+++ app/models/InventoryItem.ts
@@ -25,10 +25,10 @@ export interface InventoryItemData {
   quantity: number;
   publicCount: number;
   notes: string;
-  attributes: { [key: string]: any };
+  attributes: { [key: string]: Object };
 }
 
-export function attributesToString(attributes: { [key: string]: any }) {
+export function attributesToString(attributes: { [key: string]: Object }) {
   return Object.keys(attributes)
     .map((attributeKey) => `${attributeKey}: ${attributes[attributeKey]}`)
     .join("\n");
