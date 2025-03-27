c89dfaafee68c0ed046bfb668ec25d8f58d8359a
scf2054 <frosts1125@gmail.com>
2025-03-27 12:01:17 -0400

    Fixed landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -6,6 +6,7 @@ import RequestAccessForm from "../components/RequestAccessForm";
 import SignUpForm from "../components/SignUpForm";
 import { useState } from "react";
 import RequestSubmitted from "../components/RequestSubmitted";
+import Image from "next/image"
 
 export default function LandingPage() {
     const [currentForm, setCurrentForm] = useState(LandingFormType.LoginForm);
@@ -35,7 +36,13 @@ export default function LandingPage() {
 
     return <div className="bg-[#D3E8FF] w-screen h-screen flex items-center justify-center">
         <div className="bg-[#002856] h-screen w-screen sm:h-fit sm:w-[550px] pb-10 sm:rounded-md shadow-md flex flex-col items-center text-center">
-            <img src="/assets/Header Logo.png" alt={`"The MIF Foundation" company logo`} className="w-[353px] h-[100px] mt-5 mb-2"></img>
+            <Image 
+                src="/assets/Header Logo.png" 
+                alt={`"The MIF Foundation" company logo`} 
+                className="mt-5 mb-2"
+                width="353"
+                height="100"
+            />
             <h5 className="text-white mb-4">DME Parts Exchange Portal</h5>
             {getCurrentForm()}
         </div>
