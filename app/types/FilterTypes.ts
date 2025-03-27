9e6c2728c013f2eecc1be82dee0fe861889e2a17
scf2054 <frosts1125@gmail.com>
2025-03-27 16:08:39 -0400

    Added it back

---
+++ app/types/FilterTypes.ts
@@ -0,0 +1,17 @@
+export enum FilterType {
+  Active = "Active",
+  Part = "Part",
+  Model = "Model",
+  Width = "Width",
+  Height = "Height",
+  QuantityMin = "quantityMin",
+  QuantityMax = "quantityMax",
+  LocationLat = "locationLat",
+  LocationLong = "locationLong",
+  Radius = "radius"
+};
+
+export enum FilterComponentType {
+  LISTINGS = "LISTINGS",
+  INVENTORY_ITEMS = "INVENTORY_ITEMS"
+}
