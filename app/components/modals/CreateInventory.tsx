2744b77f425da8de503cb7af2217e6a4bb415fc0
scf2054 <frosts1125@gmail.com>
2025-03-28 12:41:25 -0400

    Added fields for description and address fields

--- app/components/modals/CreateInventory.tsx
+++ app/components/modals/CreateInventory.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import backendService from "@/app/services/backend.service";
+import { Textarea } from "@/components/ui/textarea";
 
 interface CreateInventoryModalProps {
   organizationID: number,
@@ -17,29 +18,41 @@ export default function CreateInventoryModal({organizationID, onClose}: CreateIn
     title: z
       .string()
       .min(1, "Title is required"),
-    address: z
+    description: z
+      .string(),
+    addressLine1: z
+      .string(),
+    addressLine2: z
+      .string(),
+    city: z
+      .string(),
+    state: z
+      .string(),
+    zipCode: z
       .string()
-      .regex(/(\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.)?/, "Must be in the format \"2014 Forrest Hills Dr.\"")
   });
   const createInventoryForm = useForm<z.infer<typeof createInventoryFormSchema>>({
     resolver: zodResolver(createInventoryFormSchema),
     defaultValues: {
       title: "",
-      address: ""
+      description: "",
+      addressLine1: "",
+      addressLine2: "",
+      city: "",
+      state: "",
+      zipCode: ""
     }
   })
   
   const onInventorySubmit = (values: z.infer<typeof createInventoryFormSchema>) => {
-    const body = {
-      name: values.title,
-      organizationID: organizationID,
-      description: "",
-      location: values.address
-    };
+    // const body = {
+    //   name: values.title,
+    //   organizationID: organizationID,
+    //   description: "",
+    //   location: values.address
+    // };
     // TODO: Create new address and create inventory after that's created
-    // TODO: form for address
     // TODO: loading spinner
-    // TODO: field for description
     // backendService.post(`/organizations/${organizationID}/inventories`, body)
     //   .then(response => {
     //     // TODO: toastr with message
@@ -77,6 +90,98 @@ export default function CreateInventoryModal({organizationID, onClose}: CreateIn
             />
             <FormField
               control={createInventoryForm.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="mb-[1.5rem]">
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      placeholder="Description"
+                      className="h-[5rem]"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <div>
+              <FormField
+                control={createInventoryForm.control}
+                name="addressLine1"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Address Line 1"
+                        className="mb-[0.75rem]"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={createInventoryForm.control}
+                name="addressLine2"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Address Line 2"
+                        className="mb-[0.75rem]"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={createInventoryForm.control}
+                name="city"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="City"
+                        className="mb-[0.75rem]"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between mb-[1.5rem]">
+                <FormField
+                  control={createInventoryForm.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem className="w-[48%]">
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="State"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={createInventoryForm.control}
+                  name="zipCode"
+                  render={({ field }) => (
+                    <FormItem className="w-[48%]">
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Zip Code"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            {/* <FormField
+              control={createInventoryForm.control}
               name="address"
               render={({ field }) => (
                 <FormItem>
@@ -89,7 +194,7 @@ export default function CreateInventoryModal({organizationID, onClose}: CreateIn
                   </FormControl>
                 </FormItem>
               )}
-            />
+            /> */}
             <div className="flex w-max ml-auto mt-[1.5rem]">
               <button onClick={onClose} className="button !bg-[#BBBBBB]">Cancel</button>
               <button type="submit" className="button ml-[1rem]">
