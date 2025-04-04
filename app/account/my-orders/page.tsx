bea53b3f121cfac472659a801702f099b14cd197
scf2054 <frosts1125@gmail.com>
2025-04-03 21:40:27 -0400

    My Orders done but the API calls

--- app/account/my-orders/page.tsx
+++ app/account/my-orders/page.tsx
@@ -1,68 +1,42 @@
 "use client"
 
 import KeysetPagination from "@/app/components/KeysetPagination";
+import Dialog from "@/app/components/modals/Dialog";
+import Modal from "@/app/components/modals/Modal";
 import Order from "@/app/components/Order";
 import Search from "@/app/components/Search";
-import { OrderData, Orders, OrderStatus } from "@/app/models/Order";
+import { userEmitter } from "@/app/layout";
+import { Orders, OrdersPatch, OrderStatus } from "@/app/models/Order";
 import { capitalize } from "@/app/models/Status";
+import { UserData } from "@/app/models/User";
+import backendService from "@/app/services/backend.service";
 import { FilterComponentType } from "@/app/types/FilterTypes";
-import { useEffect, useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+import { useEffect, useRef, useState } from "react";
+import { toast } from "sonner";
 
 export default function AccountMyOrders() {
   const [orders, setOrders] = useState<Orders>();
+  const [userID, setUserID] = useState("");
+  const [loadingOrders, setLoadingOrders] = useState(false);
+  const [orderStatusDialogIsOpen, setOrderStatusDialogIsOpen] = useState(false);
+  const [orderAndStatusSelected, setOrderAndStatusSelected] = useState<{orderID: number | string, status: string}>();
+
+  const searchRef = useRef<{
+    executeSearch: () => void;
+    clearSearch: () => void;
+  } | null>(null);
+
+  useEffect(() => {
+    userEmitter.on("user", (userEmitted: UserData) => {
+      setUserID(userEmitted.id);
+    });
+  });
 
   const receiveOrders = (data: object) => {
     const dataAsOrders = data as Orders;
     setOrders(dataAsOrders);
   } 
-
-  useEffect(() => { // TODO: for testing, remove after hookup
-    receiveOrders({
-        message: "Success",
-        success: true,
-        data: {
-          results: [{
-            id: 1,
-            inventoryItem: {
-              id: 1,
-              name: "Item",
-              part: {
-                id: 1,
-                name: "Part",
-                model: "",
-                description: "",
-                partNumber: "sudnbhg",
-                partType: "Part Type"
-              },
-              model: {
-                name: "Model",
-                year: 2025,
-                id: 1
-              },
-              quantity: 50,
-              publicCount: 50,
-              notes: "Notes",
-              attributes: { "Attribute 1": "attr" }
-            },
-            vendor: {
-              id: 1,
-              firstName: "First",
-              lastName: "Last",
-              email: "example@example.com",
-              displayName: "Display Name",
-              type: "admin"
-            },
-            quantity: 25,
-            createdAt: "November 25th 2001",
-            status: "Initiated"
-          }],
-          hasNextPage: false,
-          hasPreviousPage: false,
-          nextCursor: null,
-          previousCursor: null
-        }
-    })
-  }, [])
 
   const getOrderStatusMenuItems = (orderStatus: string) => {
     let items: string[] = orderStatus.toLowerCase() == OrderStatus.PENDING ? 
@@ -73,35 +47,87 @@ export default function AccountMyOrders() {
     return items;
   }
 
-  const onOrderMenuItemChange = (item: string, order: OrderData) => {
-    // TODO: API call for changing status
-    console.log(item)
+  const changeOrderStatus = async (orderID: number | string, status: string) => {
+    setLoadingOrders(true);
+    const body = {
+      status: status,
+    };
+    const response = await backendService.patch(`/orders/${orderID}`, body);
+    const responseAsOrders = response as OrdersPatch;
+    toast(responseAsOrders.message);
+    setLoadingOrders(false);
+    return responseAsOrders.success;
   }
 
-  return <>{orders && <div className="relative h-full">
-    {/**TODO: API route & searchBy */}
-    <Search 
-      apiRoute="" 
-      searchBy=""
-      receiveResponse={receiveOrders}
-      placeholderText="Search Orders"
-      filterType={FilterComponentType.ORDERS}
-    />
-    <div className="w-full px-[0.75rem] py-[2rem] max-h-[45rem] overflow-y-auto">
-      {orders.data.results.map(order => 
-        <Order
-          order={order}
-          key={order.id}
-          menuItems={order.status ? getOrderStatusMenuItems(order.status) : []}
-          onMenuItemClick={(item) => onOrderMenuItemChange(item, order)}
+  const onOrderMenuItemChange = (item: string, orderID: number | string) => {
+    setOrderAndStatusSelected({
+      orderID: orderID,
+      status: item.split("Mark as ")[1].toLowerCase()
+    });
+    setOrderStatusDialogIsOpen(true);
+  }
+
+  const onOrderStatusDialogClose = async (confirmed: boolean) => {
+    if(confirmed && orderAndStatusSelected) {
+      setOrderStatusDialogIsOpen(false);
+      await changeOrderStatus(orderAndStatusSelected.orderID, orderAndStatusSelected.status);
+      searchRef.current?.executeSearch();
+    }
+    setOrderStatusDialogIsOpen(false);
+  }
+
+  return (
+    <>
+      <div className="relative h-full">
+        {/**TODO: searchBy */}
+        {userID && (
+          <Search
+            apiRoute={`/users/${userID}/orders`}
+            searchBy=""
+            receiveResponse={receiveOrders}
+            filterType={FilterComponentType.ORDERS}
+            placeholderText="Search Orders"
+            loadingResponse={(loading) => setLoadingOrders(loading)}
+          />
+        )}
+        {orders && (
+          <>
+            <div className="w-full px-[0.75rem] py-[2rem] max-h-[45rem] overflow-y-auto">
+              {loadingOrders && <Spinner />}
+              {!loadingOrders &&
+                orders.data.results.map((order) => (
+                  <Order
+                    order={order}
+                    key={order.id}
+                    menuItems={getOrderStatusMenuItems(order.status)}
+                    onMenuItemClick={(item) => onOrderMenuItemChange(item, order.id)}
+                    className="mb-[1rem]"
+                  />
+                ))}
+            </div>
+            <KeysetPagination
+              hasNextPage={orders.data.hasNextPage}
+              hasPreviousPage={orders.data.hasPreviousPage}
+              nextCursor={orders.data.nextCursor}
+              previousCursor={orders.data.previousCursor}
+            />
+          </>
+        )}
+      </div>
+      {orderAndStatusSelected && 
+      <Modal
+        isOpen={orderStatusDialogIsOpen}
+        onClose={() => setOrderStatusDialogIsOpen(false)}
+      >
+        <Dialog
+          text={
+            `Are you sure you\'d like to mark this order as ${orderAndStatusSelected?.status}`
+          }
+          header={"Change Order Status"}
+          onClose={onOrderStatusDialogClose}
         />
-      )}
-    </div>
-    <KeysetPagination 
-      hasNextPage={orders.data.hasNextPage}
-      hasPreviousPage={orders.data.hasPreviousPage}
-      nextCursor={orders.data.nextCursor}
-      previousCursor={orders.data.previousCursor}
-    />
-  </div>}</>
+      </Modal>
+      }
+    </>
+  );
 }
