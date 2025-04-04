59e9e4d3dd5172195fe69014a5008cbae6672759
scf2054 <frosts1125@gmail.com>
2025-04-03 21:07:48 -0400

    Formatting

--- app/account/received-orders/page.tsx
+++ app/account/received-orders/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import KeysetPagination from "@/app/components/KeysetPagination";
 import Dialog from "@/app/components/modals/Dialog";
@@ -15,7 +15,7 @@ import { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 
 // MENU OPTIONS
-const HANDLE_ORDER = "Handle Order"
+const HANDLE_ORDER = "Handle Order";
 
 export default function AccountReceivedOrders() {
   const [orders, setOrders] = useState<Orders>();
@@ -27,100 +27,109 @@ export default function AccountReceivedOrders() {
 
   const router = useRouter();
   const searchRef = useRef<{
-    executeSearch: () => void,
-    clearSearch: () => void
+    executeSearch: () => void;
+    clearSearch: () => void;
   } | null>(null);
 
   const receiveOrders = (data: object) => {
     const dataAsOrders = data as Orders;
     setOrders(dataAsOrders);
-  } 
+  };
 
   useEffect(() => {
     userEmitter.on("user", (userEmitted: UserData) => {
       setUserID(userEmitted.id);
-      if(userEmitted.organization) {
+      if (userEmitted.organization) {
         setOrgID(userEmitted.organization.id);
       } else {
         router.push("/listings"); // route somewhere else?
       }
-    })
-  })
+    });
+  });
 
   const handleOrder = async (orderID: number | string) => {
     setLoadingOrders(true);
     const body = {
-      providerId: userID
-    }
+      providerId: userID,
+    };
     const response = await backendService.patch(`/orders/${orderID}`, body);
     const responseAsOrders = response as OrdersPatch;
     toast(responseAsOrders.message);
     setLoadingOrders(false);
     return responseAsOrders.success;
-  }
+  };
 
   const onMenuItemClick = (item: string, order: OrderData) => {
-    switch(item) {
+    switch (item) {
       case HANDLE_ORDER:
         setOrderHandling(order.id);
         setHandleOrderDialogIsOpen(true);
         break;
     }
-  }
+  };
 
   const onHandleOrderDialogClose = async (confirm: boolean) => {
-    if(confirm && orderHandling) {
+    if (confirm && orderHandling) {
       setHandleOrderDialogIsOpen(false);
       await handleOrder(orderHandling);
       searchRef.current?.executeSearch();
     }
     setHandleOrderDialogIsOpen(false);
-  }
+  };
 
-  return <>
-  <div className="relative h-full">
-    {/**TODO: searchBy, change apiRoute to POOL when fixed */}
-    {orgID && <Search 
-      apiRoute={`/organizations/${orgID}/orders`} 
-      searchBy=""
-      receiveResponse={receiveOrders}
-      placeholderText="Search Orders"
-      loadingResponse={(loading) => setLoadingOrders(loading)}
-    />}
-    {orders && <>
-    <div className="w-full px-[0.75rem] py-[2rem] max-h-[45rem] overflow-y-auto">
-      {loadingOrders && <Spinner />}
-      {!loadingOrders && orders.data.results.map(order => 
-        <Order
-          order={order}
-          key={order.id}
-          menuItems={[HANDLE_ORDER]}
-          onMenuItemClick={(item) => onMenuItemClick(item, order)}
-          className="mb-[1rem]"
-          onStatusClick={() => {
-            setOrderHandling(order.id)
-            setHandleOrderDialogIsOpen(true)
-          }}
+  return (
+    <>
+      <div className="relative h-full">
+        {/**TODO: searchBy, change apiRoute to POOL when fixed */}
+        {orgID && (
+          <Search
+            apiRoute={`/organizations/${orgID}/orders`}
+            searchBy=""
+            receiveResponse={receiveOrders}
+            placeholderText="Search Orders"
+            loadingResponse={(loading) => setLoadingOrders(loading)}
+          />
+        )}
+        {orders && (
+          <>
+            <div className="w-full px-[0.75rem] py-[2rem] max-h-[45rem] overflow-y-auto">
+              {loadingOrders && <Spinner />}
+              {!loadingOrders &&
+                orders.data.results.map((order) => (
+                  <Order
+                    order={order}
+                    key={order.id}
+                    menuItems={[HANDLE_ORDER]}
+                    onMenuItemClick={(item) => onMenuItemClick(item, order)}
+                    className="mb-[1rem]"
+                    onStatusClick={() => {
+                      setOrderHandling(order.id);
+                      setHandleOrderDialogIsOpen(true);
+                    }}
+                  />
+                ))}
+            </div>
+            <KeysetPagination
+              hasNextPage={orders.data.hasNextPage}
+              hasPreviousPage={orders.data.hasPreviousPage}
+              nextCursor={orders.data.nextCursor}
+              previousCursor={orders.data.previousCursor}
+            />
+          </>
+        )}
+      </div>
+      <Modal
+        isOpen={handleOrderDialogIsOpen}
+        onClose={() => setHandleOrderDialogIsOpen(false)}
+      >
+        <Dialog
+          text={
+            'Are you sure you\'d like to handle this order? It will appear under the "My Orders" tab on your profile.'
+          }
+          header={"Handle Order"}
+          onClose={onHandleOrderDialogClose}
         />
-      )}
-    </div>
-    <KeysetPagination 
-      hasNextPage={orders.data.hasNextPage}
-      hasPreviousPage={orders.data.hasPreviousPage}
-      nextCursor={orders.data.nextCursor}
-      previousCursor={orders.data.previousCursor}
-    />
-    </>}
-  </div>
-  <Modal
-    isOpen={handleOrderDialogIsOpen}
-    onClose={() => setHandleOrderDialogIsOpen(false)}
-  >
-    <Dialog 
-      text={"Are you sure you'd like to handle this order? It will appear under the \"My Orders\" tab on your profile."}
-      header={"Handle Order"}
-      onClose={onHandleOrderDialogClose}
-    />
-  </Modal>
-  </>
+      </Modal>
+    </>
+  );
 }
